a8f10bf191a0a1205de3c59a468c72efe713cf72
Thanh2901 <thanhvuworkspace@gmail.com>
2025-02-04 23:21:07 +0700

    add config service, discovery service

--- backend/finance_service/src/main/java/com/example/finance_service/FinanceServiceApplication.java
+++ backend/finance_service/src/main/java/com/example/finance_service/FinanceServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.finance_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class FinanceServiceApplication {
 
     public static void main(String[] args) {
