6a55ad8441eb7c3de291d23c16b9f43a082fa806
Thanh2901 <thanhvuworkspace@gmail.com>
2025-02-06 23:48:53 +0700

    update admin server, micrometer + zipkin

---
+++ backend/monitor_admin/src/test/java/com/example/monitor_admin/MonitorAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitor_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
