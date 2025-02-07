cd3e4bc9439d84501a73803ab74861e981894eeb
Thanh2901 <thanhvuworkspace@gmail.com>
2025-02-07 10:17:39 +0700

    update micrometer, zipkin for all, recover old request mapping of all services

--- backend/tenant_service/src/main/java/com/example/tenant_service/controller/TenantController.java
+++ backend/tenant_service/src/main/java/com/example/tenant_service/controller/TenantController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@RequestMapping("/tenant")
+@RequestMapping("/tenant")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
 public class TenantController {
